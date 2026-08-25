a369422a8f6ece9cdc71af66f499cbc313e35b19
oceanspr <osnetway@gmail.com>
2026-08-25 16:43:37 +0100

    Updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { ThemeProvider } from "./context/ThemeContext";
 import Editor from "./Editor";
+import ConsentBanner from "./components/ConsentBanner/ConsentBanner";
+import CommentForm from "./components/Comments/CommentForm";
+import CommentList from "./components/Comments/CommentList";
+import Header from "./components/Layout/Header";
+import Footer from "./components/Layout/Footer";
 
 export default function App() {
   return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Community Newsletter Editor</h1>
-      <Editor />
-    </div>
+    <ThemeProvider>
+      <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+        <Header />
+        
+        <h1>Community Newsletter Editor</h1>
+        <Editor />
+
+        {/* Privacy & Consent Banner */}
+        <ConsentBanner />
+
+        {/* Comments Section */}
+        <section style={{ marginTop: "2rem" }}>
+          <h2>Community Comments</h2>
+          <CommentForm />
+          <CommentList />
+        </section>
+
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
